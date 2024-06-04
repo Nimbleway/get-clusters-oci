8c021d8e595be7eaa4681f04633cd472275b9de3
Eyal Halfon <eyalh@nimbleway.com>
2024-06-04 08:41:14 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+GET_ALL="${GET_ALL:-false}"
+
+if [ $GET_ALL = true ]; then
+  CLUSTERS=$(oci ce cluster list --compartment-id $COMPARTMENT_ID --lifecycle-state ACTIVE --query "data[].id")
+  str=$(echo $CLUSTERS | tr -d '"' | tr -d '\' | tr -d '[' | tr -d ']')
+  IFS=$'\n' read -r -d '' -a MODIFY <<< $str
+  RESULT=$(jq -n '$ARGS.positional' --args "${MODIFY[@]}")
+  echo $RESULT
+  echo "ocids=$(echo $RESULT)" >> $GITHUB_OUTPUT
+else
+  CLUSTERS=$(oci ce cluster list --compartment-id $COMPARTMENT_ID --lifecycle-state ACTIVE --query "data[?contains(\"name\",'$CLUSTER_NAME')] | [?contains(\"name\",'$ENVIRONMENT')] .id")
+  str=$(echo $CLUSTERS | tr -d '"' | tr -d '\' | tr -d '[' | tr -d ']')
+  IFS=$'\n' read -r -d '' -a MODIFY <<< $str
+  RESULT=$(jq -n '$ARGS.positional' --args "${MODIFY[@]}")
+  echo $RESULT
+  echo "ocids=$(echo $RESULT)" >> $GITHUB_OUTPUT
+fi
