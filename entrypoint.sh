3294e0030352916bc61bf1e64585ffeb71dd3aa1
Eyal Halfon <eyalh@nimbleway.com>
2024-06-04 09:44:47 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,17 +24,17 @@ fi
 GET_ALL="${GET_ALL:-false}"
 
 if [ $GET_ALL = true ]; then
-  CLUSTERS=$(oci ce cluster list --compartment-id $COMPARTMENT_ID --lifecycle-state ACTIVE --query "data[].id")
-  str=$(echo $CLUSTERS | tr -d '"' | tr -d '\' | tr -d '[' | tr -d ']')
-  IFS=$'\n' read -r -d '' -a MODIFY <<< $str
-  RESULT=$(jq -n '$ARGS.positional' --args "${MODIFY[@]}")
-  echo $RESULT
-  echo "ocids=$(echo $RESULT)" >> $GITHUB_OUTPUT
+  CLUSTERS=$(oci ce cluster list --compartment-id ocid1.compartment.oc1..aaaaaaaafnrvqjgsvwytt42sx6mxrmg7xcedrn36akmytcq2pjh4uir2puja --lifecycle-state ACTIVE --query "data[].id")
+  #str=$(echo $CLUSTERS | tr -d '"' | tr -d '\' | tr -d '[' | tr -d ']')
+  #IFS=$'\n' read -r -d '' -a MODIFY <<< $str
+  #RESULT=$(jq -n '$ARGS.positional' --args "${MODIFY[@]}")
+  echo $CLUSTERS
+  echo "ocids=$(echo $CLUSTERS)" >> $GITHUB_OUTPUT
 else
   CLUSTERS=$(oci ce cluster list --compartment-id $COMPARTMENT_ID --lifecycle-state ACTIVE --query "data[?contains(\"name\",'$CLUSTER_NAME')] | [?contains(\"name\",'$ENVIRONMENT')] .id")
-  str=$(echo $CLUSTERS | tr -d '"' | tr -d '\' | tr -d '[' | tr -d ']')
-  IFS=$'\n' read -r -d '' -a MODIFY <<< $str
-  RESULT=$(jq -n '$ARGS.positional' --args "${MODIFY[@]}")
-  echo $RESULT
-  echo "ocids=$(echo $RESULT)" >> $GITHUB_OUTPUT
+  # str=$(echo $CLUSTERS | tr -d '"' | tr -d '\' | tr -d '[' | tr -d ']')
+  # IFS=$'\n' read -r -d '' -a MODIFY <<< $str
+  # RESULT=$(jq -n '$ARGS.positional' --args "${MODIFY[@]}")
+  echo $CLUSTERS
+  echo "ocids=$(echo $CLUSTERS)" >> $GITHUB_OUTPUT
 fi
