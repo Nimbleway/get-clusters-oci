c600ac5e84c2871bf31009f0b117af910d79594c
Eyal Halfon <eyalh@nimbleway.com>
2024-06-04 09:13:26 +0300

    oci cli

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-chmod +x ./setup-oci-cli.sh
-./setup-oci-cli.sh
+if ! command -v oci &> /dev/null
+then
+    echo "OCI CLI not found, installing..."
+    # Install OCI CLI
+    bash -c "$(curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh)"
+else
+    echo "OCI CLI is already installed"
+fi
+
 
 GET_ALL="${GET_ALL:-false}"
 
