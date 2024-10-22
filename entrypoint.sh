c9a403c116d78086da35b2f9e99cbe4cd6190504
Yotam Nachum <yotamn@nimbleway.com>
2024-10-22 15:35:32 +0300

    Use login shell

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 
 if [ $GET_ALL = true ]; then
   CLUSTERS=$(oci ce cluster list --compartment-id ocid1.compartment.oc1..aaaaaaaafnrvqjgsvwytt42sx6mxrmg7xcedrn36akmytcq2pjh4uir2puja --lifecycle-state ACTIVE --query "data[].id")
@@ -6,5 +6,5 @@ else
   CLUSTERS=$(oci ce cluster list --compartment-id $COMPARTMENT_ID --lifecycle-state ACTIVE --query "data[?contains(\"name\",'$CLUSTER_NAME')] | [?contains(\"name\",'$ENVIRONMENT')] .id")
 fi
 
-echo "ocids=${CLUSTERS}"
-echo "ocids=${CLUSTERS}" >> $GITHUB_OUTPUT
+echo "ocids=$(echo $CLUSTERS)"
+echo "ocids=$(echo $CLUSTERS)" >> $GITHUB_OUTPUT
